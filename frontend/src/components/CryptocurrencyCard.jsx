19096e63fda9eb2efa4eb20db776a21a21e97f13
n1ghtmare-dev <oboev3691@gmail.com>
2025-04-12 18:48:11 +0300

    Initial commit: base project setup

---
+++ frontend/src/components/CryptocurrencyCard.jsx
@@ -0,0 +1,26 @@
+import { Card } from "antd"
+
+
+function CryptocurrencyCard(props) {
+
+  const { currency } = props
+
+  const price = Math.round(currency.quote.USD.price)
+
+  return (
+    <div>
+      <Card title={
+        <div className="flex items-center gap-3">
+          <img src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${currency.id}.png`} />
+          < span > {currency.name}</span>
+        </div>
+      }
+        style={{ width: 300 }}>
+        <p>Текущая цена: {price}</p>
+        <p>Card content</p>
+      </Card >
+    </div >
+  )
+}
+
+export default CryptocurrencyCard
